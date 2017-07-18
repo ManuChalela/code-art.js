3cd4c004f0f1c1e743a5e0a7b8a523c6d3683ff2
ManuChalela <emanuelchalela@gmail.com>
2017-07-18 17:51:53 -0300

    Add identifiers function and create an array of identifiers and their number of times they appear

--- code.js
+++ code.js
@@ -1,25 +1,35 @@
 const esprima = require('esprima');
-var source = 'answer = 42; hola = 5; isCold = "Si";';
+var source = 'answer = 42; hola = 5; isCold = "Si"; answer = 50;';
 const tokens = esprima.tokenize(source);
-console.log(tokens);
+//console.log(tokens);
 
 var identificadores = tokens.filter(function (el) {
     return (el.type === "Identifier");
 });
 console.log("El código es: ");
 console.log(source);
-console.log("Los identificadores son: ");
-console.log(identificadores);
-
+//console.log("Los identificadores son: ");
+//console.log(identificadores);
+function arrayObjectIndexOf(myArray, searchTerm, property) {
+    for(var i = 0, len = myArray.length; i < len; i++) {
+        if (myArray[i][property] === searchTerm) return i;
+    }
+    return -1;
+}
 var armarArray = [];
 console.log("El array de Identificadores es: ");
 identificadores.forEach(function (element) {
-    if(armarArray.indexOf(element.value) == -1){
-        armarArray.push({ "id": element.value});
-    } else {
-        var indice = armarArray.indexOf(element.value);
-        console.log(indice);
+    var indice = arrayObjectIndexOf(armarArray,element.value, "id");
+    if(indice == -1){ // No encontró el identificador, lo agrega.
+        armarArray.push(new Identifier(element.value,1));
+    } else { // Encontró el identificador, suma uno.
+        armarArray[indice].quantity++;
     }
 });
 
 console.log(armarArray);
+
+function Identifier(id, quantity) {
+    this.id = id;
+    this.quantity = quantity;
+}
