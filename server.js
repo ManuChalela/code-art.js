88cf775ceb8d403ba2cfd825c271566d72155daa
ManuChalela <emanuelchalela@gmail.com>
2017-10-17 01:06:07 -0300

    express server.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 var fs = require('fs');
 var index = fs.readFileSync('code-art-graph.html');
 
@@ -8,3 +8,32 @@ http.createServer(function(req, res) {
   });
   res.end(index);
 }).listen(8080);
+*/
+
+
+var express = require("express");
+var app = express();
+var path = require("path");
+var cons = require('consolidate');
+var jsonfile = require('jsonfile');
+
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html')
+
+app.listen(8080, () => {
+  console.log("Server started!");
+});
+
+app.get("/", function(req, res) {
+  res.render('graph')
+});
+
+
+app.get("/getNodes", (req, res) => {
+  console.log("Get nodes");
+  jsonfile.readFile('views/graph.json', (err, obj) => {
+    //res.send(JSON.stringify(obj));
+    res.send(obj);
+  });
+});
